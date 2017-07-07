a54826b54566f0b13961f7031b2b3ba6e99090ef
y-kurami <yosuke.kurami@gmail.com>
2017-07-07 12:56:31 +0900

    remove comment out

--- packages/reg-gh-app/deploy.sh
+++ packages/reg-gh-app/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# yarn run webpack
-# cp serverless.yml serverless.yml.bk
-# cat serverless.yml.bk | sed "s/- serverless-webpack//" > serverless.yml
-# yarn run sls -- deploy
-# mv serverless.yml.bk serverless.yml
+yarn run webpack
+cp serverless.yml serverless.yml.bk
+cat serverless.yml.bk | sed "s/- serverless-webpack//" > serverless.yml
+yarn run sls -- deploy
+mv serverless.yml.bk serverless.yml
 apiEndpoint=$(./node_modules/.bin/sls info | grep POST - | head -n 1 | cut -b 10- | sed "s/\/api.*//")
 cat << JSON > ../reg-notify-github-plugin/.endpoint.json
 { "endpoint": "$apiEndpoint" }
