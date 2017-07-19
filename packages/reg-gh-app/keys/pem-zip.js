588ae546e68be6698d1cccd8563435c3acf71502
y-kurami <yosuke.kurami@gmail.com>
2017-07-19 11:42:41 +0900

    oops

--- packages/reg-gh-app/keys/pem-zip.js
+++ packages/reg-gh-app/keys/pem-zip.js
@@ -17,18 +17,21 @@ function decode() {
     console.error("export GH_APP_PRIVATE_KEY");
     process.exit(1);
   }
-  if (fs.statSync(keyPath)) {
-    const x = encode();
-    if (x === v) {
-      console.log("private-key.pem already exists");
-      process.exit(0);
-    } else {
-      console.warn("private-key.pem already exists, but values are different.");
-      process.exit(1);
+  try {
+    if (fs.statSync(keyPath)) {
+      const x = encode();
+      if (x === v) {
+        console.log("private-key.pem already exists");
+        process.exit(0);
+      } else {
+        console.warn("private-key.pem already exists, but values are different.");
+        process.exit(1);
+      }
     }
+  } catch (e) {
+    const p = zlib.inflateSync(new Buffer(v, "base64"));
+    fs.writeFileSync(keyPath, p);
   }
-  const p = zlib.inflateSync(new Buffer(v, "base64"));
-  fs.writeFileSync(keyPath, p);
 }
 
 if (process.argv[2] === "encode") {
