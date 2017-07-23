e44d384eb11e84cbd48fd75f9945cfb040df174c
y-kurami <yosuke.kurami@gmail.com>
2017-07-23 12:16:01 +0900

    use file schema instead of http

--- packages/reg-suit-toppage/capture.js
+++ packages/reg-suit-toppage/capture.js
@@ -1,25 +1,18 @@
-const express = require("express");
 const Nightmare = require("nightmare");		
-const mkdirp = require("mkdirp");
-
-const app = express();
 const nightmare = Nightmare({ show: false, width: 1500, height: 3400 });
 
+const mkdirp = require("mkdirp");
 mkdirp.sync(`${__dirname}/screenshot`);
-app.use(express.static("dist"))
 
-const server = app.listen(3000, () => {
-  nightmare
-    .goto("http://localhost:3000")
-    .then(() => console.log("Open top page."))
-    .then(() => nightmare.wait(100))
-    .then(() => nightmare.screenshot(`${__dirname}/screenshot/index.png`))
-    .then(() => console.log("Captured screenshot."))
-    .then(() => nightmare.end())
-    .then(() => server.close())
-    .catch(x => {
-      console.error(x);
-      process.exit(1);
-    })
-  ;
-})
+nightmare
+  .goto(`file://${__dirname}/dist/index.html`)
+  .then(() => console.log("Open top page."))
+  .then(() => nightmare.wait(100))
+  .then(() => nightmare.screenshot(`${__dirname}/screenshot/index.png`))
+  .then(() => console.log("Captured screenshot."))
+  .then(() => nightmare.end())
+  .catch(x => {
+    console.error(x);
+    process.exit(1);
+  })
+;
