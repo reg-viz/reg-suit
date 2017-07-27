2cb25217cfdefc6f7a7662b8965c5d7ba965414e
y-kurami <yosuke.kurami@gmail.com>
2017-07-27 18:35:43 +0900

    fix build script

--- packages/reg-gh-app-front/webpack.config.js
+++ packages/reg-gh-app-front/webpack.config.js
@@ -1,9 +1,27 @@
 const path = require("path");
 const webpack = require("webpack");
-const Dotenv = require('dotenv-webpack');
+const Dotenv = require("dotenv-webpack");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = function(env) {
-  const systemvars = (env && env === "prod");
+  const plugins = (env && env === "prod") ? [
+    new webpack.LoaderOptionsPlugin({
+      tsConfigPath: path.resolve(__dirname, "src/tsconfig.build.json"),
+    }),
+    new Dotenv({
+      path: path.resolve(__dirname, "../../.env"),
+      systemvars: true,
+    }),
+    new UglifyJSPlugin(),
+  ] : [
+    new webpack.LoaderOptionsPlugin({
+      tsConfigPath: path.resolve(__dirname, "src/tsconfig.build.json"),
+    }),
+    new Dotenv({
+      path: path.resolve(__dirname, "../../.env"),
+      systemvars: false,
+    }),
+  ];
   return {
     entry: {
       main: path.resolve(__dirname, "src/index.ts"),
@@ -22,15 +40,7 @@ module.exports = function(env) {
         { test: /\.graphql$/, loader: "raw-loader" },
       ],
     },
-    plugins: [
-      new webpack.LoaderOptionsPlugin({
-        tsConfigPath: path.resolve(__dirname, "src/tsconfig.build.json"),
-      }),
-      new Dotenv({
-        path: path.resolve(__dirname, "../../.env"),
-        systemvars,
-      }),
-    ],
+    plugins,
     devServer: {
       port: 4000,
       contentBase: path.join(__dirname, "public"),
