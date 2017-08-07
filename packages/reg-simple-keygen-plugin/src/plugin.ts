9fb6c80b30633460dbf2235b06e8697f1126712f
y-kurami <yosuke.kurami@gmail.com>
2017-08-07 18:04:28 +0900

    Fix logger and warn messages

--- packages/reg-simple-keygen-plugin/src/plugin.ts
+++ packages/reg-simple-keygen-plugin/src/plugin.ts
@@ -9,21 +9,22 @@ export class SimpleKeygenPlugin implements KeyGeneratorPlugin<PluginConfig> {
   _logger: PluginLogger;
   _options: PluginConfig;
   init(config: PluginCreateOptions<PluginConfig>): void {
+    this._logger = config.logger;
     this._options = config.options;
   }
 
   getExpectedKey(): Promise<string> {
     if (!this._options.expectedKey || !this._options.expectedKey.length) {
-      this._logger.error("Expected key must be non null string.");
-      return Promise.reject<string>(new Error("Expected key must be non null string."));
+      this._logger.warn("Expected key must be non null string.");
+      return Promise.reject<string>(null);
     }
     return Promise.resolve(this._options.expectedKey);
   }
 
   getActualKey(): Promise<string> {
     if (!this._options.actualKey || !this._options.actualKey.length) {
-      this._logger.error("Actual key must be non null string.");
-      return Promise.reject<string>(new Error("Actual key must be non null string."));
+      this._logger.warn("Actual key must be non null string.");
+      return Promise.reject<string>(null);
     }
     return Promise.resolve(this._options.actualKey);
   }
