777c803a51f42c8f4f09d2fbec0a33b1b9c5a437
y-kurami <yosuke.kurami@gmail.com>
2017-09-21 04:00:29 +0900

    workarounds: move conf.js

--- packages/reg-gh-app-front/cmrh.conf.js
+++ packages/reg-gh-app-front/cmrh.conf.js
@@ -1,3 +1,6 @@
-module.exports = {
-  generateScopedName: '[name]_[local]',
-};
+// FIXME
+// This file is copyed to the repository root dir because workspaces feature hoisted css-modules-require-hook and it can't this file.
+//
+// module.exports = {
+//   generateScopedName: '[name]_[local]',
+// };
