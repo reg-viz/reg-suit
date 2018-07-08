2055df26503b4c5fec7b165881f46f928e28e015
Yosuke Kurami <yosuke.kurami@gmail.com>
2018-07-09 01:03:05 +0900

    Add plugin

--- packages/reg-notify-gitlab-plugin/src/index.ts
+++ packages/reg-notify-gitlab-plugin/src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello, reg-notify-gitlab-plugin");
+import { NotifierPluginFactory } from "reg-suit-interface";
+import { GitLabNotifierPlugin } from "./gitlab-notifier-plugin";
+import { GitLabPreparer } from "./gitlab-preparer";
+
+const factory: NotifierPluginFactory = () => {
+  return {
+    notifier: new GitLabNotifierPlugin(),
+    preparer: new GitLabPreparer(),
+  };
+};
+
+export = factory;
