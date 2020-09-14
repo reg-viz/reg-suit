8f09b7043be2c8d61a6bb3f00129cf7609ceeaec
Quramy <yosuke.kurami@gmail.com>
2020-09-14 20:57:28 +0900

    chore: extract jest config js

---
+++ packages/reg-notify-github-plugin/jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "globals": {
+    "ts-jest": {
+      "diagnostics": false
+    }
+  },
+  "transform": {
+    "^.+\\.ts$": "ts-jest"
+  },
+  "testRegex": "(src/.*\\.test)\\.ts$",
+  "testPathIgnorePatterns": [
+    "/node_modules/",
+    "\\.d\\.ts$",
+    "lib/.*"
+  ],
+  "collectCoverageFrom": [
+    "src/**/*.ts",
+    "!**/testing/**"
+  ],
+  "moduleFileExtensions": [
+    "js",
+    "ts",
+    "json"
+  ]
+};
