4147b305c4a387facc51d68bdc3be8d50fa230b8
Quramy <yosuke.kurami@gmail.com>
2024-01-20 03:03:27 +0900

    feat(s3-plugin): Remove prepare option to create bucket

--- packages/reg-publish-s3-plugin/src/s3-bucket-preparer.ts
+++ packages/reg-publish-s3-plugin/src/s3-bucket-preparer.ts
@@ -1,104 +1,29 @@
-import { v4 as uuid } from "uuid";
-import { CreateBucketCommand, PutBucketPolicyCommand, S3Client } from "@aws-sdk/client-s3";
 import { PluginPreparer, PluginCreateOptions, PluginLogger } from "reg-suit-interface";
 import { PluginConfig } from "./s3-publisher-plugin";
 
 export interface SetupInquireResult {
-  createBucket: boolean;
-  bucketName?: string;
+  bucketName: string;
 }
-
-function createPolicy(bucketName: string) {
-  return {
-    Version: "2012-10-17",
-    Id: "Policy1498486961145",
-    Statement: [
-      {
-        Sid: "Stmt1498486956732",
-        Effect: "Allow",
-        Principal: "*",
-        Action: "s3:GetObject",
-        Resource: `arn:aws:s3:::${bucketName}/*`,
-      },
-    ],
-  };
-}
-
-const BUCKET_PREFIX = "reg-publish-bucket";
 
 export class S3BucketPreparer implements PluginPreparer<SetupInquireResult, PluginConfig> {
-  private _s3client = new S3Client();
   _logger!: PluginLogger;
 
   inquire() {
     return [
       {
-        name: "createBucket",
-        type: "confirm",
-        message: "Create a new S3 bucket",
-        default: true,
-      },
-      {
         name: "bucketName",
         type: "input",
-        message: "Existing bucket name",
-        when: (ctx: any) => !(ctx as { createBucket: boolean }).createBucket,
+        message: "Bucket name",
       },
     ];
   }
 
-  prepare(config: PluginCreateOptions<SetupInquireResult>) {
+  async prepare(config: PluginCreateOptions<SetupInquireResult>) {
     this._logger = config.logger;
-    const ir = config.options;
-    if (!ir.createBucket) {
-      return Promise.resolve({
-        bucketName: ir.bucketName as string,
-      });
-    } else {
-      const id = uuid();
-      const bucketName = `${BUCKET_PREFIX}-${id}`;
-      if (config.noEmit) {
-        this._logger.info(`Skip to create S3 bucket ${bucketName} because noEmit option.`);
-        return Promise.resolve({ bucketName });
-      }
-      this._logger.info(`Create new S3 bucket: ${this._logger.colors.magenta(bucketName)}`);
-      const spinner = this._logger.getSpinner(`creating bucket...`);
-      spinner.start();
-      return this._createBucket(bucketName)
-        .then(bucketName => {
-          return this._updatePolicy(bucketName);
-        })
-        .then(bucketName => {
-          spinner.stop();
-          return { bucketName };
-        });
-    }
-  }
-
-  _updatePolicy(bucketName: string) {
-    return new Promise<string>((resolve, reject) => {
-      this._s3client
-        .send(
-          new PutBucketPolicyCommand({
-            Bucket: bucketName,
-            Policy: JSON.stringify(createPolicy(bucketName)),
-          }),
-        )
-        .then(() => resolve(bucketName))
-        .catch(err => reject(err));
-    });
-  }
-
-  _createBucket(bucketName: string) {
-    return new Promise<string>((resolve, reject) => {
-      this._s3client
-        .send(
-          new CreateBucketCommand({
-            Bucket: bucketName,
-          }),
-        )
-        .then(() => resolve(bucketName))
-        .catch(err => reject(err));
-    });
+    const { bucketName } = config.options;
+    const pluginConfig: PluginConfig = {
+      bucketName,
+    };
+    return pluginConfig;
   }
 }
